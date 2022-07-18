ea129035d321fa30a3167afdf96177bdd2bd8960
vincentiusar <vincentiusarnoldfridolin@gmail.com>
2022-07-18 14:54:33 +0700

    Created: auth JWT and DB

---
+++ database/migrations/2022_07_18_073827_hotels.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->string('lat')->nullable();
+            $table->string('lot')->nullable();
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
